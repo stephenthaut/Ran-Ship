25e7466bc7e67ffb11501ee10eca44fcce09c6ce
stephenthaut <stephen.thaut@gmail.com>
2010-11-06 16:36:51 +1000

    Container.py

--- Container.py
+++ Container.py
@@ -4,19 +4,3 @@ class Container(object):
 	def __init__(self, variables):
 		self.__dict__ = variables
 	
-# Testing:
-# __init__()
-TestContainer = Container({"name":"Test", "colour":"Red"})
-print TestContainer.name   # => "Test"
-print TestContainer.colour # => "Red"
-
-# super_init()
-class Thing(Container):
-	def __init__(self, variables):
-		super(Thing, self).__init__(variables)
-	
-	# Other function definitions ...
-
-TestThing = Thing({"name":"Test 2", "colour":"Blue"})
-print TestThing.name   # => "Test 2"
-print TestThing.colour # => "Blue"
